01539b6b70a829404b2c3b3f15d37e3b6e6e61d1
Alex N <xander.nesta@gmail.com>
2023-02-01 15:26:15 -0500

    add animations for links and burger icon

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+    
     burger.addEventListener('click', ()=>{
+        //To Toggle Nav
         nav.classList.toggle('nav-active');
-        
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${(index / 5) + 0.3}s`
+            
+            }   
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
     }); 
+    
 }
 navSlide();
